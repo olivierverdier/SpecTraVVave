2a1c2b9e8fe4281920c5e03affbb08f3c2f9307b
SomeStrangeGuy <fierri505@gmail.com>
2013-08-12 14:47:41 +0200

    Some code changed
    to correct computation time and the resulting vector
    STATUS: unfortunate

--- codes1/travWh.py
+++ codes1/travWh.py
@@ -1,23 +1,22 @@
 def travWh(c, uini, N, tolerance):
     import math
     import numpy as np
-    from fractions import Fraction
-    
+    from scipy import linalg
+ 
     L = math.pi
-    h = L/N
-    
-    x = np.arange(h/2,L,h)
+    h = float(L)/N
+    x = np.arange(h/2.,L,h)
     xi = range(0,N,1)
     
-    ww = math.sqrt(Fraction(1, N)) * np.ones((N,1))
-    ww[0,0] = math.sqrt(Fraction(1, N))
+    ww = math.sqrt(2/float(N)) * np.ones((N,1))
+    ww[0,0] = math.sqrt(1/float(N))
 
     Tau = np.zeros((N,N))
     for m in range(N):
         for n in range(N):
             Tau[m,n] = ww[0]*ww[0]*math.cos(x[n]*xi[0])*math.cos(x[m]*xi[0])
             for k in range(1,N):
-                Tau[m,n] = Tau[m,n]+math.sqrt(Fraction(1,xi[k])*math.tanh(xi[k]))*ww[k]*ww[k]*math.cos(x[n]*xi[k])*math.cos(x[m]*xi[k])
+                Tau[m,n] = Tau[m,n] + math.sqrt(1./xi[k]*math.tanh(xi[k]))*ww[k]*ww[k]*math.cos(x[n]*xi[k])*math.cos(x[m]*xi[k])
                 
     ScriptL = -c*np.eye(N) + Tau
     
@@ -26,12 +25,10 @@ def travWh(c, uini, N, tolerance):
     it = 0
     
     while change > tolerance:
-        
         if it > 15000:
             break
-        
         DFu = ScriptL + np.diag(3*(u+1)**(0.5)-3)
-        corr = -np.linalg.solve(DFu, np.dot(ScriptL,u) + 2*(u+1)**(1.5)-3*u-2)
+        corr = np.dot(-linalg.inv(DFu),(np.dot(ScriptL,u) + 2*(u+1)**(1.5)-3*u-2))
         unew = u + corr
         change = corr.max()
         u = unew
