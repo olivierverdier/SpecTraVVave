38bf46e6e653974f50c08c24aefc8d5d3dd7097a
SomeStrangeGuy <fierri505@gmail.com>
2013-08-21 12:10:25 +0200

    evolution code
    This code computes the evolution of the given solution in time and
    therefore helps to check if the solution is correct (periodic travelling
    wave).

---
+++ codes1/evoltest.py
@@ -0,0 +1,9 @@
+# Evolution code which tests whether the computed
+# periodic travelling-wave branch is the solution 
+# for the given PDE.
+
+import math
+import numpy as np
+import routns         # file contains function computing nonlinearity and disperion 
+import time
+
