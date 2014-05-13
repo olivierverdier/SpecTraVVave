509d435119d8eabda61431edd248afe0f5bb89c2
Olivier Verdier <olivier.verdier@gmail.com>
2014-05-13 17:53:45 +0200

    new version of navigation with stepper and iterator

--- codes3/navigation.py
+++ codes3/navigation.py
@@ -1,18 +1,55 @@
 from __future__ import division
 
 import numpy as np
+from functools import partial
+import itertools
 
     
-def compute_line(p1, p2, step):
+def ortho_direction(p1, p2, step):
     """
     Return pstar such that
-        pstar = p2+(1+d)*(p2-p1)
+        pstar = p2 + step*(p2-p1)
     """
     dp = np.array([p2[0] - p1[0], p2[1] - p1[1]])
     # orthogonal direction
-    ortho = (-dp[1], dp[0])
-    
+    direction = (-dp[1], dp[0])
+
     pstar = np.array([p2[0] + dp[0]*step, p2[1] + dp[1]*step])
-    return pstar, ortho
+    return pstar, direction
+
+def step(solver, current, p1, p2, step_function, compute_line):
+    pstar, direction = compute_line(p1, p2, step_function())
+    new, p3 = solver(current, pstar, direction)
+    return new, p2, p3
+
+def get_stepper(solver, compute_line, step_function):
+    def stepper(current, p1, p2):
+         return step(current=current, p1=p1, p2=p2, solver=solver, compute_line=compute_line, step_function=step_function)
+    return stepper
+
+def get_iterator(initial, p1, p2, stepper):
+    current = initial
+    yield current, p1, p2
+    for i in itertools.count(): # infinite iterator
+        current, p2, p3 = stepper(current, p1, p2)
+        yield current, p2, p3
+
+class Navigator(object):
+    """
+    Run the iterator and stores the result.
+    """
+
+    def __init__(self, stepper):
+        self.stepper = stepper
+
+    def initialize (current, p, p0):
+        self.store = []
+        self.store.append((current, p, p0))
+
+    def run(self, N):
+        current, p2, p1 = self.store[-1]
+        iterator = get_iterator(current, p1, p2, self.stepper)
+        for result in islice(iterator, N):
+            self.state.append(result[0], result[2], result[1])
 
 
--- codes3/test_navigation.py
+++ codes3/test_navigation.py
@@ -5,18 +5,37 @@ from __future__ import division
 import unittest
 import numpy.testing as npt
 
+import itertools
+
 from navigation import *
 
-class TestNavigation(unittest.TestCase):
+def dummy_solver(x, pstar, direction):
+    return x + 1., (pstar[0]+.5*direction[0], pstar[1]+.5*direction[1])
+
+def dummy_step_function(*args, **kwargs):
+    return 1.
+
+class TestDummyNavigation(unittest.TestCase):
     def test_main(self):
         p0 = (0,0)
         p1 = (1,0)
         dp = (p1[0]-p0[0],p1[1]-p0[1])
-        computed = compute_line(p0, p1, step=1)
+        computed = ortho_direction(p0, p1, step=1)
         expected_pstar = (2, 0)
         computed_pstar = computed[0]
         npt.assert_allclose(computed_pstar, expected_pstar)
         expected_direction = (0,1)
         computed_direction = computed[1]
         self.assertEqual(computed_direction[0]*dp[0] + computed_direction[1]*dp[1], 0)
+
+    def test_stepper(self):
+        stepper = get_stepper(solver=dummy_solver, compute_line=ortho_direction, step_function=dummy_step_function)
+        print stepper(0., (0.,1.), (0., 2))
+
+    def test_iterator(self):
+        stepper = get_stepper(solver=dummy_solver, compute_line=ortho_direction, step_function=dummy_step_function)
+        iterator = get_iterator(0., (0.,1.), (0.,2.), stepper=stepper)
+        for val in itertools.islice(iterator, None, 5):
+            print val
+
 
