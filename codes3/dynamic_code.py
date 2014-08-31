f74f20cf8e8f090aa7600cafe4cf382ecdffbc24
Daulet Moldabayev <daulet.msu@gmail.com>
2014-08-31 14:09:21 +0200

    created DeFrutos class in dynamic_code.py

--- codes3/dynamic_code.py
+++ codes3/dynamic_code.py
@@ -84,3 +84,122 @@ class Dynamic_code(object):
             t = t + dt
         print 'The error for', periods,' periods is E = ', np.linalg.norm(u-solution)     
         return u
+class Defrutos_sanzserna(Dynamic_code):
+
+    
+    def multipliers(self, dt = 0.001):
+        beta = ( 2 + 2**(1/3) + 2**(-1/3) )/3
+        N = 2*self.eq.size
+        p = self.eq.degree()-1
+        
+        k = np.concatenate((np.arange(1, N/2+1, 1), np.arange(1-N/2, 0, 1)))
+        kerne = 1j*k*self.eq.compute_kernel(k) 
+        kernel = np.concatenate(([0], kerne))
+        kernel[N/2] = 0
+        k = np.concatenate((np.arange(0, N/2, 1), [0], np.arange(1-N/2, 0, 1)))    
+        m = -0.5*dt*kernel
+        
+        m1 = 1./( 1 - beta*m );
+        m2 = ( 1.5*beta*dt*1j*k/(2*(p+1)) )*m1; 
+        mm1 = 1./( 1 - (1-2*beta)*m );
+        mm2 = ( 1.5*(1-2*beta)*dt*1j*k/(2*(p+1)) )*mm1;
+        
+        return m1, m2, mm1, mm2
+    
+    def integrator(self, wave_profile, m1, m2, mm1, mm2):
+        beta = ( 2 + 2**(1/3) + 2**(-1/3) )/3
+        
+        p = self.eq.degree()-1
+        u = wave_profile
+
+        #  ---------- STEP ONE ------------ #
+
+        Y = fft(u)
+        Z = Y
+        LP = m1*Y
+        for j in range(5):
+            Z = LP - m2*( fft( ifft(Z).real**(p+1) )  )
+            
+        Y = 2*Z - Y
+        Z = Y
+        LP = mm1*Y    
+        for j in range(5):
+            Z = LP - mm2*( fft( ifft(Z).real**(p+1) )  )        
+                        
+        Y = 2*Z - Y
+        Z = Y
+        LP = m1*Y
+        for j in range(5):
+            Z = LP - m2*( fft( ifft(Z).real**(p+1) )  )
+            
+        unew = ifft( 2*Z - Y ).real
+        
+        #  ---------- STEP TWO ------------ #                                     
+                                                                  
+        Y = fft(unew)                                                                                                
+        Z = .5*( (2 + beta)*Y - beta*fft(u) )
+        LP = m1*Y                                                                                                                    
+        for j in range(5):
+            Z = LP - m2*( fft( ifft(Z).real**(p+1) )  )
+        
+        Z = .5*( Y + (2-beta)*fft(unew) - (1-beta)*fft(u) )
+        LP = mm1*Y                                                                                                                                                                                                                                                                                                                                                            
+        for j in range(5):
+            Z = LP - mm2*( fft( ifft(Z).real**(p+1) )  )
+    
+        Y = 2*Z - Y
+        Z = .5*( Y + 2*fft(unew) - fft(u) )
+        LP = m1*Y
+        for j in range(5):
+            Z = LP - m2*( fft( ifft(Z).real**(p+1) )  )
+    
+        uold = u
+        u = unew
+        unew = ifft( 2*Z - Y ).real
+    
+        #  ---------- STEP THREE ------------ #
+    
+        q1 = .5*beta*(1+beta); q2 = beta*(2+beta); q3 = .5*(2+beta)*(1+beta);
+        qq1 = .5*(2-beta)*(1-beta); qq2 = (3-beta)*(1-beta); qq3 = .5*(3-beta)*(2-beta);
+        
+        for j in range(1):
+            Q1 = fft(q1*uold - q2*u + q3*unew)
+            Q2 = fft(qq1*uold - qq2*u + qq3*unew)
+            Q3 = fft(uold - 3*u + 3*unew)
+            
+            Y = fft(unew)
+            Z = .5*( Y + Q1 )
+            LP = m1*Y
+            for k in range(2):
+                Z = LP - m2*( fft( ifft(Z).real**(p+1) )  )
+            
+            Y = 2*Z - Y
+            Z = .5*( Y + Q2 )
+            LP = mm1*Y
+            for k in range(2):
+                Z = LP - mm2*( fft( ifft(Z).real**(p+1) )  )
+    
+            Y = 2*Z - Y
+            Z = .5*( Y + Q3 )
+            LP = m1*Y
+            for k in range(2):
+                Z = LP - m2*( fft( ifft(Z).real**(p+1) )  )
+    
+            uold = u; u = unew
+            unew = ifft( 2*Z - Y ).real
+        
+        u = unew
+        return u
+    
+    def evolution(self, solution, dt = 0.001, periods = 1):
+        u = solution    
+              
+        T = 2*self.eq.length/self.velocity
+        t = dt
+        m1, m2, mm1, mm2 = self.multipliers(dt=dt)
+        while t < periods*T+dt:
+            w = self.integrator(wave_profile = u, m1=m1, m2=m2, mm1=mm1, mm2=mm2)
+            u = w
+            t = t + dt
+        print 'The error for', periods,' periods is E = ', np.linalg.norm(u-solution)     
+        return u
