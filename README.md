845bdaa0d8d37fc30fe7151dad4a17ebe81e53d1
Olivier Verdier <olivier.verdier@gmail.com>
2014-10-17 17:05:22 +0200

    minimum requirement for github

---
+++ README.md
@@ -0,0 +1,1 @@
+# `travwave`: Compute traveling waves
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+
+setup(
+	name = 'travwave',
+	description = 'Compute traveling waves',
+	author='Daulet Moldabayev, Henrik Kalisch, Olivier Verdier',
+	license = 'MIT',
+	keywords = ['Math'],
+	
+	packages=['travwave'],
+	classifiers = [
+	'Development Status :: 3 - Alpha',
+	'Environment :: Console',
+	'Intended Audience :: Science/Research',
+	'License :: OSI Approved :: MIT License',
+	'Operating System :: OS Independent',
+	'Programming Language :: Python',
+	'Topic :: Scientific/Engineering :: Mathematics',
+	zip_safe = False,
+	)
