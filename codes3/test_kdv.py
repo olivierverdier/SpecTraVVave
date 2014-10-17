8fe55ee67d47239c7de9b3bc28ffa90c50e74c04
Olivier Verdier <olivier.verdier@gmail.com>
2014-10-17 10:58:08 +0200

    test KdV with long length with analytic soliton

--- codes3/test_kdv.py
+++ codes3/test_kdv.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 def sech(x):
     return 1/np.cosh(x)
 
-def exact_kdv(amplitude, x):
+def soliton(amplitude, x):
     s = sech(np.sqrt(3*amplitude)/2*x)
     return amplitude*s*s
 
@@ -35,11 +35,15 @@ def derivative2(xs, us):
 
 class TestKDV(unittest.TestCase):
 
+    def get_length(self):
+        return 10
+
+    def get_nbsteps(self):
+        return 1
 
     def setUp(self):
-        size = 128
-        length = 10
-        ## length = np.pi
+        size = 256
+        length = self.get_length()
         self.equation = self.get_equation_class()(size, length)
         self.boundary = self.get_boundary()
         solver = Solver(self.equation, self.boundary)
@@ -47,20 +51,25 @@ class TestKDV(unittest.TestCase):
         initial_guess = self.equation.compute_initial_guess()
         initial_velocity = self.equation.bifurcation_velocity()
         p1 = (initial_velocity, 0)
-        epsilon = .1
+        nb_steps = self.get_nbsteps()
+        epsilon = .1/nb_steps
         p0 = (initial_velocity, -epsilon)
         nav.initialize(initial_guess, p1, p0)
-        nav.run(1)
+        nav.run(nb_steps)
         self.nav = nav
         store = nav.store[-1]
         self.B = store[1][0]
         self.c = store[2][0]
+        self.amplitude = store[2][1]
         self.xs = self.equation.nodes
         self.computed = store[0]
 
+    def get_residual_tolerance(self):
+        return 1e-6
+
     def test_residual(self):
         res = self.residual(self.c, self.B, self.xs, self.computed)
-        npt.assert_allclose(res, 0, atol=1e-6)
+        npt.assert_allclose(res, 0, atol=self.get_residual_tolerance())
 
     def test_boundary(self):
         level = self.boundary.level
@@ -78,3 +87,29 @@ class TestKDV(unittest.TestCase):
         u = identity(xs,us)
         res = (1-c) * u + 3/4*u*u + 1/6*derivative2(xs,us) - B
         return res
+        
+class TestKDVSoliton(TestKDV):
+    def get_length(self):
+        return 50
+
+    def get_nbsteps(self):
+        return 1
+
+    def get_residual_tolerance(self):
+        return 1e-5
+
+    def get_boundary(self):
+        return Minimum(0)
+
+    def test_soliton(self):
+        expected = soliton(self.amplitude, self.xs)
+        npt.assert_allclose(self.computed, expected, atol=1e-3)
+
+@unittest.skip("Takes too long")
+class TestKDVSolitonLarge(TestKDVSoliton):
+    def get_length(self):
+        return 70
+
+    def get_nbsteps(self):
+        return 50
+
