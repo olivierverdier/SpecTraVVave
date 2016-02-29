b1734dca22e8c8b62d1369bdfbed535218425da6
Olivier Verdier <olivier.verdier@gmail.com>
2016-02-29 20:55:35 +0100

    test both dynamic solvers

--- test/test_dynamic.py
+++ test/test_dynamic.py
@@ -11,37 +11,18 @@ from travwave.dynamic import *
 from travwave.diagram import *
 
 
-class TestTrapezoidal(unittest.TestCase):
-    def test(self):
-        length = 3*np.pi
-        equation = kdv.KDV(length)
-        boundary_cond = Const()
-        bd = BifurcationDiagram(equation, boundary_cond, doublings=1)
-        bd.navigation.run(10)
-
-        u = bd.navigation[-2]['solution']
-        velocity = bd.navigation[-2]['current'][bd.navigation.velocity_]
-        dyn = Trapezoidal_rule(equation, u, velocity)
-        uu = dyn.mirror()
-        t_wave = dyn.evolution(solution = uu, nb_steps=int(1e2), periods = 1)
-
-        error = t_wave - uu
-        print(max(abs(error)))
-        npt.assert_allclose(t_wave, uu, atol=1e-3, err_msg="Wave is equal to itself after travelling one period")
-
-class TestDeFrutos(unittest.TestCase):
-    @unittest.skip("code duplication")
-    def test(self):
-        size = 128
+class TestDynamic(unittest.TestCase):
+    def test_dynamid(self):
         length = 3*np.pi
         equation = kdv.KDV(length)
         boundary_cond = Const()
         bd = BifurcationDiagram(equation, boundary_cond)
+        bd.initialize()
         bd.navigation.run(10)
 
         u = bd.navigation[-1]['solution']
         velocity = bd.navigation[-1]['current'][bd.navigation.velocity_]
-        dyn = Trapezoidal_rule(equation, u, velocity)
+        dyn = self.get_dynamic_class()(equation, u, velocity)
         uu = dyn.mirror()
         t_wave = dyn.evolution(solution = uu, nb_steps=int(1e2), periods = 1)
 
@@ -49,3 +30,9 @@ class TestDeFrutos(unittest.TestCase):
         print(max(abs(error)))
         npt.assert_allclose(t_wave, uu, atol=1e-3, err_msg="Wave is equal to itself after travelling one period")
 
+    def get_dynamic_class(self):
+        return Trapezoidal_rule
+
+class TestDefrutos(TestDynamic):
+    def get_dynamic_class(self):
+        return DeFrutos_SanzSerna
