f1831b445c8ab04f638e634760f00a691d8511cb
Olivier Verdier <olivier.verdier@gmail.com>
2016-06-03 11:20:57 +0200

    add abstraction Equation methods

--- travwave/equations/base.py
+++ travwave/equations/base.py
@@ -5,3 +5,9 @@ from __future__ import division
 class Equation(object):
     def __init__(self, length):
         self.length = length
+
+    def compute_kernel(self, k):
+        raise NotImplementedError()
+
+    def flux(self, u):
+        raise NotImplementedError()
